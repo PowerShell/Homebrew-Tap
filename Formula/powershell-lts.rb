f4c7c392921b85b5d44a51ed9d716dfd58712aaf
Travis Plunk <github@ez13.net>
2025-03-27 10:50:03 -0700

    Update powershell-lts.rb

--- Formula/powershell-lts.rb
+++ Formula/powershell-lts.rb
@@ -1,62 +1,71 @@
-# typed: false
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-# frozen_string_literal: true
+name: PowerShell LTS PR
 
-# Doc for the class.
-# Class to implement Brew Formula to install PowerShell
-class PowershellLts < Formula
-  desc "Formula to install PowerShell Long Term Stable Channel"
-  homepage "https://github.com/powershell/powershell"
+permissions: read-all
 
-  @arm64url = "https://github.com/PowerShell/PowerShell/releases/download/v7.2.9/powershell-7.2.9-osx-arm64.tar.gz"
-  @x64url = "https://github.com/PowerShell/PowerShell/releases/download/v7.2.9/powershell-7.2.9-osx-x64.tar.gz"
-  @arm64sha256 = "d34572d97ef4002b361fdedac51a9bca39b4b2d1e526e7355de062063ae9f8bf"
-  @x64sha256 = "4b6ca38156561d028ad346ad7539592c04ea2c09bfdf6da59b3a72a1dd39d2ee"
+on:
+  push:
+    paths:
+      - '.github/workflows/PR-powershell-lts.yml'
+      - 'Formula/powershell-lts.rb'
+    branches:
+      - master
+  pull_request:
 
-  # We do not specify `version "..."` as 'brew audit' will complain - see https://github.com/Homebrew/legacy-homebrew/issues/32540
-  if Hardware::CPU.intel?
-    url @x64url
-    # must be lower-case
-    sha256 @x64sha256
-  else
-    url @arm64url
-    # must be lower-case
-    sha256 @arm64sha256
-  end
+defaults:
+  run:
+    shell: pwsh
+env:
+  FORMULA_PATH: ./Formula/powershell-lts.rb
+  UPDATE_SCRIPT_PATH: ./scripts/Upgrade-Formula.ps1
+  FORMULA_NAME: powershell-lts
+  CHANNEL_NAME: lts
+  HOMEBREW_NO_ENV_HINTS: 1
+  HOMEBREW_NO_INSTALL_CLEANUP: 1
 
-  version "7.2.9"
-  version_scheme 1
+jobs:
+  changes:
+    if: startsWith(github.repository_owner, 'azure') || github.repository_owner == 'PowerShell'
+    name: Change Detection
+    runs-on: ubuntu-latest
+    # Required permissions
+    permissions:
+      pull-requests: read
+    # Set job outputs to values from filter step
+    outputs:
+      source: ${{ steps.filter.outputs.workflows }}
+    steps:
+      - name: checkout
+        uses: actions/checkout@v4
 
-  # .NET Core 3.1 requires High Sierra - https://docs.microsoft.com/en-us/dotnet/core/install/dependencies?pivots=os-macos&tabs=netcore31
-  depends_on macos: :high_sierra
+        # For pull requests it's not necessary to checkout the code
+      - uses: dorny/paths-filter@de90cc6fb38fc0963ad72b210f1f284cd68cea36 # v3.2.0
+        id: filter
+        with:
+          list-files: json
+          filters: |
+            workflows:
+              - '.github/workflows/PR-powershell-lts.yml'
+              - 'Formula/powershell-lts.rb'
 
-  def install
-    libexec.install Dir["*"]
-    chmod 0555, libexec/"pwsh"
-    bin.install_symlink libexec/"pwsh" => "pwsh-lts"
-  end
+      - name: Capture outputs
+        run: |
+          "workflows: ${{ steps.filter.outputs.workflows }}"
+        shell: pwsh
+      
+  homebrew-formula-lts:
+    timeout-minutes: 15
+    needs: changes
+    if: ${{ needs.changes.outputs.workflows == 'true' }}
+    runs-on: macos-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v4
 
-  def caveats
-    <<~EOS
-      The executable should already be on PATH so run with `pwsh-lts`. If not, the full path to the executable is:
-        #{bin}/pwsh-lts
+      - name: Prepare Agent
+        uses: ./.github/workflows/composite/prep
 
-      Other application files were installed at :
-        #{libexec}
-
-      If you also have the Cask installed, you need to run the following to make the formula your default install:
-        brew link --overwrite powershell
-
-      If you would like to make PowerShell LTS your default shell, run
-        sudo echo '#{bin}/pwsh-lts' >> /etc/shells
-        chsh -s #{bin}/pwsh-lts
-    EOS
-  end
-
-  test do
-    assert_equal "7.2.9",
-                 shell_output("#{bin}/pwsh-lts -c '$psversiontable.psversion.tostring()'").strip
-  end
-end
+      - name: Install and Test Formula
+        uses: ./.github/workflows/composite/installAndTest
